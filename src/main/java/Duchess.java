e3c8b9541be00d2759088bc3c04337254163afe8
Dexter Kwxn <dexter.kwxn@gmail.com>
2025-01-22 12:49:06 +0800

    Add greet and exit message

---
+++ src/main/java/Duchess.java
@@ -0,0 +1,23 @@
+public class Duchess {
+    public static void main(String[] args) {
+        String logo = "   ___                    _                              \n"
+                +"  |   \\   _  _     __    | |_      ___     ___     ___   \n"
+                +"  | |) | | +| |   / _|   | ' \\    / -_)   (_-<    (_-<   \n"
+                +"  |___/   \\_,_|   \\__|_  |_||_|   \\___|   /__/_   /__/_  \n"
+              +"_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n"
+              +"`-0-0-\'\"`-0-0-\'\"`-0-0-\'\"`-0-0-\'\"`-0-0-\'\"`-0-0-\'\"`-0-0-\'\n";
+        String chatbotName = "Duchess";
+        String chatLine = "________________________________\n";
+        String greetingMsg = "Hello! I'm " + chatbotName + "\n" + 
+                        "What can I do for you?";
+        String exitMsg = "Bye. Hope to see you again soon!\n";
+
+        System.out.println(logo);
+        System.out.println(chatLine);
+        System.out.println(greetingMsg);
+        System.out.println(chatLine);
+        System.out.println(exitMsg);
+        System.out.println(chatLine);
+    }
+
+}
